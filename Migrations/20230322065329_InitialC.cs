f4afe96a186489def182809d4033e2d389cdec4a
Dielectric-i <ruesinc@gmail.com>
2023-03-22 15:14:19 +0600

    22032023
     Сделал из Поля players - лист
     Удалил лишний код
     Настроил зависимость Player/Game многие ко многим, поправил соответственно репозиторий и GameService
     Добавил проверку в Move что переданный игрок существует

---
+++ Migrations/20230322065329_InitialC.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Sense_Capital_XOGameApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Player1Symbol = table.Column<string>(type: "longtext", nullable: false),
+                    Player2Symbol = table.Column<string>(type: "longtext", nullable: false),
+                    CurrentPlayerId = table.Column<int>(type: "int", nullable: false),
+                    WinnerId = table.Column<int>(type: "int", nullable: true),
+                    BoardState = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "GamePlayer",
+                columns: table => new
+                {
+                    GamesId = table.Column<int>(type: "int", nullable: false),
+                    PlayersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GamePlayer", x => new { x.GamesId, x.PlayersId });
+                    table.ForeignKey(
+                        name: "FK_GamePlayer_Games_GamesId",
+                        column: x => x.GamesId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamePlayer_Players_PlayersId",
+                        column: x => x.PlayersId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Moves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Row = table.Column<int>(type: "int", nullable: false),
+                    Column = table.Column<int>(type: "int", nullable: false),
+                    PlayerId = table.Column<int>(type: "int", nullable: false),
+                    GameId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moves", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Moves_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Moves_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlayer_PlayersId",
+                table: "GamePlayer",
+                column: "PlayersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_GameId",
+                table: "Moves",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_PlayerId",
+                table: "Moves",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GamePlayer");
+
+            migrationBuilder.DropTable(
+                name: "Moves");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
--- Repositories/GameRepository.cs
+++ Repositories/GameRepository.cs
@@ -17,47 +17,55 @@ namespace Sense_Capital_XOGameApi.Repositories
         // Create new game
         public async Task<Game> CreateGameAsync(Game game)
         {
-                if (game == null)
-                {
-                    throw new ArgumentNullException($"The game parameter is null");
-                }
-                await _context.Games.AddAsync(game);
-                await _context.SaveChangesAsync();
-                return game;
+            if (game == null)
+            {
+                throw new ArgumentNullException($"The game parameter is null");
+            }
+            await _context.Games.AddAsync(game);
+            await _context.SaveChangesAsync();
+            game = await GetGameAsync(game.Id);
+            return game;
         }
 
         // Get game by id
         public async Task<Game> GetGameAsync(int id)
         {
-                var game = await _context.Games.FindAsync(id);
-                if (game == null)
-                {
-                    throw new ArgumentException($"Game with id {id} not found.");
-                }
-                return game;
+            if (!int.TryParse(id.ToString(), out int parsedId))
+            {
+                throw new ArgumentException($"Invalid game id: {id}");
+            }
+            var game = await _context.Games
+        .Include(g => g.Players)
+        .Include(g => g.Moves)
+        .FirstOrDefaultAsync(g => g.Id == id);
+            if (game == null)
+            {
+                throw new ArgumentException($"Game with id {id} not found.");
+            }
+            return game;
         }
 
         // Get all games
         public async Task<IEnumerable<Game>> GetAllGamesAsync()
         {
-                var games = await _context.Games.ToListAsync();
-                return games;
+            var games = await _context.Games.ToListAsync();
+            return games;
         }
 
         //Delete game by Id
         public async Task DeleteAsync(int id)
         {
-                var game = await GetGameAsync(id);
-                _context.Games.Remove(game);
-                await _context.SaveChangesAsync();
+            var game = await GetGameAsync(id);
+            _context.Games.Remove(game);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<Game> UpdateAsync(Game game)
         {
-                //var game = await GetGameAsync(id);
-                _context.Games.Update(game);
-                _context.SaveChanges();
-                return game;
+            //var game = await GetGameAsync(id);
+            _context.Games.Update(game);
+            _context.SaveChanges();
+            return game;
         }
     }
 }
