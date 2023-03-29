7a579e53a49a3ddf959c0711bfcea929870e4fac
Dielectric-i <ruesinc@gmail.com>
2023-03-30 04:05:38 +0600

    годится для примера)

---
+++ Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Sense_Capital_XOGameApi.Filters
+{
+    public class ErrorHandlingFilterAttribute:ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+          var exception = context.Exception;
+
+            var errorResult = new { error = "An error occurred while processing you request." };
+            context.Result = new ObjectResult(errorResult)
+            {
+                StatusCode = 500
+            };
+
+            context.ExceptionHandled= true;
+
+        }
+    }
+}
