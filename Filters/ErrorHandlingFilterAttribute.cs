16bd173e10de13defeac6da1d975c132ab1d800d
Dielectric-i <ruesinc@gmail.com>
2023-03-30 11:43:13 +0600

    EditFilter
    Add problemDetails to handling

--- Filters/ErrorHandlingFilterAttribute.cs
+++ Filters/ErrorHandlingFilterAttribute.cs
@@ -1,21 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
 
 namespace Sense_Capital_XOGameApi.Filters
 {
-    public class ErrorHandlingFilterAttribute:ExceptionFilterAttribute
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
     {
         public override void OnException(ExceptionContext context)
         {
-          var exception = context.Exception;
+            var exception = context.Exception;
 
-            var errorResult = new { error = "An error occurred while processing you request." };
-            context.Result = new ObjectResult(errorResult)
+            var problemDetails = new ProblemDetails
             {
-                StatusCode = 500
+                Type = "http://tools.ietf.org/html/rfc7231#section-6.6.1",
+                Title = "An error occurred while processing your request. ",
+                Status = (int)HttpStatusCode.InternalServerError,
             };
+            context.Result = new ObjectResult(problemDetails);
 
-            context.ExceptionHandled= true;
+            context.ExceptionHandled = true;
 
         }
     }
